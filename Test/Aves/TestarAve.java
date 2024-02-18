340a958f1cdf5e643855c02ce50a1be07499b1b1
Vitor 0-0 <75373270+VitorBuenoOliveira@users.noreply.github.com>
2024-02-18 11:58:35 -0300

    JavaUpdate3

--- Test/Aves/TestarAve.java
+++ Test/Aves/TestarAve.java
@@ -2,11 +2,15 @@ package Aves;
 
 public class TestarAve {
 	public static void main(String[] args) {
-		Aves canario = new Aves(0, 0, 0);
+		Aves canario = new Aves(1,5,10);
 		System.out.println(canario.peso);
 		
-		Aves sabia = new Aves(0, 0, 0);
+		Aves sabia = new Aves(5,7,15);
 		System.out.println(sabia.cantar());
+		
+		Aves gaviao = new Aves(1500,30,1500);
+		System.out.println(gaviao.cantar());
+		System.out.println(gaviao.peso);
 		
 	}
 }
