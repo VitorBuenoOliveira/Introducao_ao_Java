3c8e7e4ec99cd2e02fdc8f3f39511628229ea01f
Vitor 0-0 <75373270+VitorBuenoOliveira@users.noreply.github.com>
2024-03-06 21:20:43 -0300

    savejava5.1

--- Lista2/ApenasCaracteres.java
+++ Lista2/ApenasCaracteres.java
@@ -1,12 +1,18 @@
-package Lista2;
+// package Lista2;
 
-public class ApenasCaracteres {
-    public static void main(String[] args){
+// public class ApenasCaracteres {
+//     public static void main(String[] args){
 
-    }
+//     }
 
-    public static boolean IsNumero(String ver){
-        int PrimeiroC = 0;
+//     public static boolean IsNumero(String ver){
+//         int[] num = new int[]{
+//             0,1,2,3,4,5,6,7,8,9
+//         };
+//         String PrimeiroC = ver;
+//         if(PrimeiroC == num){
+//             return true;
+//         }
 
-    }
-}
+//     }
+// }
